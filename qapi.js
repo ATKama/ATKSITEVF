1fb85478aed5901f3ca47162188620e8c6312a48
ATKama <132297166+ATKama@users.noreply.github.com>
2025-04-14 23:51:54 +0200

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,75 @@
+export default {
+	"pages": {
+		"root": {
+			"name": "root",
+			"children": [
+				"67fa69a79710450018a84f31",
+				"67fa69a79710450018a84f35",
+				"67fa69a79710450018a84f39",
+				"67fa69a79710450018a84f41",
+				"67fa69a79710450018a84f3d"
+			],
+			"id": "root",
+			"pageUrl": "root"
+		},
+		"67fa69a79710450018a84f31": {
+			"id": "67fa69a79710450018a84f31",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"67fa69a79710450018a84f35": {
+			"id": "67fa69a79710450018a84f35",
+			"name": "Accueil",
+			"pageUrl": "index",
+			"seo": {
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"title": "Home | Website Example",
+				"og:title": "Home | Website Example",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		},
+		"67fa69a79710450018a84f39": {
+			"id": "67fa69a79710450018a84f39",
+			"pageUrl": "about",
+			"name": "À propos",
+			"seo": {
+				"og:title": "About | Website Example",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "About | Website Example"
+			}
+		},
+		"67fa69a79710450018a84f3d": {
+			"id": "67fa69a79710450018a84f3d",
+			"seo": {
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Work | Website Example",
+				"og:title": "Work | Website Example",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			},
+			"pageUrl": "work",
+			"name": "Générateur IA"
+		},
+		"67fa69a79710450018a84f41": {
+			"id": "67fa69a79710450018a84f41",
+			"seo": {
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Team | Website Example",
+				"og:title": "Team | Website Example"
+			},
+			"pageUrl": "team",
+			"name": "Services"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
